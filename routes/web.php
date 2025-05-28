175bc2d9b03678adc73e81a6a213b385a23de895
Necrology / Maulana Gandawijaya <necrology69@gmail.com>
2025-05-28 22:50:34 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('landing');
 });
 
+Route::get('/test-env', function () {
+    return env('VITE_DEV_SERVER_URL');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -14,7 +18,3 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/{any}', function () {
     return view('home'); // Tampilkan Blade yang berisi Vue
 })->where('any', '.*');
-
-Route::get('/test-env', function () {
-    return env('VITE_DEV_SERVER_URL');
-});
