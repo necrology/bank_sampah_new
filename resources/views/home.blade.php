7282611728ed97cb4c809d2ee90e58fe43a7db1d
Necrology / Maulana Gandawijaya <necrology69@gmail.com>
2025-05-28 23:22:06 +0700

    dmsakl

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="landing/assets/img/logo.png" />
+    @if (app()->environment('local'))
+    <script type="module" src="http://202.10.47.115:5173/@vite/client"></script>
+    <script type="module" src="http://202.10.47.115:5173/resources/js/app.js"></script>
+    @else
     @vite('resources/js/app.js')
+    @endif
 </head>
 
 <body>
