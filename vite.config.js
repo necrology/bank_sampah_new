e1308024cc43fc6b4f70b23e9990d223488d7a00
Necrology / Maulana Gandawijaya <necrology69@gmail.com>
2025-05-28 23:15:48 +0700

    dsa

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.js"],
-            refresh: true,
-        }),
-        vue(),
-    ],
-    server: {
-        host: "202.10.47.115",
-        port: "5173",
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    const devServerUrl = env.VITE_DEV_SERVER_URL || "http://localhost:5173";
+    const url = new URL(devServerUrl);
+
+    return {
+        plugins: [
+            laravel({
+                input: ["resources/js/app.js"],
+                refresh: true,
+                devServer: devServerUrl,
+            }),
+            vue(),
+        ],
+        server: {
+            host: url.hostname,
+            port: Number(url.port),
+        },
+    };
 });
